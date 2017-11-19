7e230a936e48b5bedb0d49fd6fc7510eb364ef16
tony <antoabrand@gmail.com>
2017-11-18 22:59:27 -0600

    adding some better readability in output

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,15 @@
 function fizzBuzz(num) {
   for (var i = 1; i < num + 1; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log("fizzBuzz");
+      console.log("FizzBuzz: " + i +" " + " is - divisible by 3 and 5");
     } else if (i % 3 === 0) {
-      console.log("fizz");
+      console.log("Fizz: " + i +" " + " is - divisible by 3");
     } else if (i % 5 === 0) {
-      console.log("buzz");
+      console.log("Buzz: " + i +" " + " is - divisible by 5");
     } else {
       console.log(i);
     }
   }
 }
 
-fizzBuzz(100);
+fizzBuzz(50);
