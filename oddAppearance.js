0e468b927255a7d4222f4e23bd6964197228623c
tony <antoabrand@gmail.com>
2017-11-20 18:37:03 -0600

    adding some comments

--- oddAppearance.js
+++ oddAppearance.js
@@ -1,19 +1,23 @@
 function oddAppearance(inputList, type) {
   var inputListArray = inputList.split(" ");
   var inputListObj = {};
-  
+
+  /* 
+    foreach item in the array - populate obj with item and initialize with value
+    0 if item does not exist in the obj, then increment the value by one 
+    each time we come across the item again
+  */
   inputListArray.forEach(itemInList => {
     if (!inputListObj[itemInList]) inputListObj[itemInList] = 0;
     inputListObj[itemInList]++;
   });
 
   for (let item in inputListObj) {
-    if (type === 'odd' && inputListObj[item] % 2 !== 0) {
-      console.log(item + ': appears odd amount of times');
-    }
-    else if (type === 'even' && inputListObj[item] % 2 === 0){
-        console.log(item + ': appears even amount of times')
+    if (type === "odd" && inputListObj[item] % 2 !== 0) {
+      console.log(item + ": appears odd amount of times");
+    } else if (type === "even" && inputListObj[item] % 2 === 0) {
+      console.log(item + ": appears even amount of times");
     }
   }
 }
-oddsOrEvens('a b a b b c', 'odd');
+oddsOrEvens("a b a b b c", "odd");
