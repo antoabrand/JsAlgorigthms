1ce9a139904c77c1f184507540f4ab1e96bf544b
tony <antoabrand@gmail.com>
2017-11-20 18:44:02 -0600

    a little more robust

--- oddAppearance.js
+++ oddAppearance.js
@@ -1,25 +1,27 @@
 function oddAppearance(inputList, type) {
-  var inputListArray = inputList.split(" ");
-  var inputListObj = {};
+  let inputListArray = inputList.split(" ");
+  let inputListObj = {};
 
   /* 
     foreach item in the array - populate obj with item and initialize with value
     0 if item does not exist in the obj, then increment the value by one 
     each time we come across the item again
   */
-  inputListArray.forEach(itemInList => {
-    if (!inputListObj[itemInList]) inputListObj[itemInList] = 0;
-    inputListObj[itemInList]++;
-  });
+  if (inputList) {
+    inputListArray.forEach(itemInList => {
+      if (!inputListObj[itemInList]) inputListObj[itemInList] = 0;
+      inputListObj[itemInList]++;
+    });
 
-  for (let item in inputListObj) {
-    if (type === "odd" && inputListObj[item] % 2 !== 0) {
-      console.log(item + ": appears odd amount of times");
-    } else if (type === "even" && inputListObj[item] % 2 === 0) {
-      console.log(item + ": appears even amount of times");
+    for (let item in inputListObj) {
+      if (type === "odd" && inputListObj[item] % 2 !== 0) {
+        console.log(item + ": appears odd amount of times");
+      } else if (type === "even" && inputListObj[item] % 2 === 0) {
+        console.log(item + ": appears even amount of times");
+      }
     }
   }
 }
 
-oddAppearance('a b a b b c', 'odd');
+oddAppearance("a b a b b c", "odd");
 //oddAppearance('a b a b b c', 'even');
