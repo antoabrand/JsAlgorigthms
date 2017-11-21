014765f6183a3f0528badb2dbde09994cdf36452
tony <antoabrand@gmail.com>
2017-11-20 18:55:31 -0600

    cleaning upstrings

--- oddAppearance.js
+++ oddAppearance.js
@@ -9,19 +9,21 @@ function oddAppearance(inputList, type) {
   */
   if (inputList) {
     inputListArray.forEach(itemInList => {
-      if (!inputListObj[itemInList]) inputListObj[itemInList] = 0;
+      if (!inputListObj[itemInList]) {
+        inputListObj[itemInList] = 0;
+      }
       inputListObj[itemInList]++;
     });
 
     for (let item in inputListObj) {
-      if (type === "odd" && inputListObj[item] % 2 !== 0) {
-        console.log(item + ": appears odd amount of times");
-      } else if (type === "even" && inputListObj[item] % 2 === 0) {
-        console.log(item + ": appears even amount of times");
+      if (type === 'odd' && inputListObj[item] % 2 !== 0) {
+        console.log(item + ': appears odd amount of times');
+      } else if (type === 'even' && inputListObj[item] % 2 === 0) {
+        console.log(item + ': appears even amount of times');
       }
     }
   }
 }
 
-oddAppearance("a b a b b c", "odd");
+oddAppearance('a b a b b c', 'odd');
 //oddAppearance('a b a b b c', 'even');
