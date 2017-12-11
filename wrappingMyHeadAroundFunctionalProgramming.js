d217716dc2b13f1f9a8b60e6ee3602c4b9901252
tony <antoabrand@gmail.com>
2017-12-10 20:05:08 -0600

    tidying up

---
+++ wrappingMyHeadAroundFunctionalProgramming.js
@@ -0,0 +1,25 @@
+function mapForEach(arr, aFunc) {
+    newArray = [];
+    for (item of arr) {
+        if(item !== undefined){       
+         newArray.push(aFunc(item))}
+    }
+    return newArray;
+}
+
+var intialArray = [1, 2, 3];
+console.log(intialArray);
+
+var initialArrayTimes2 = mapForEach(intialArray, function (item) {
+    return item * 2;
+})
+console.log(initialArrayTimes2);
+
+var oddItemsInInitialArray = mapForEach(intialArray, function(item){
+    if (item % 2 !== 0){
+        return item;
+    }
+    return
+})
+
+console.log(oddItemsInInitialArray);
