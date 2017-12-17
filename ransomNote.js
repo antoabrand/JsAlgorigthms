aa3a90eea7b527541a9a4732a7e4923f6ff2b2ba
antoabrand <aaandrade1987@gmail.com>
2017-12-17 12:17:48 -0600

    removing uneeded for loop

--- ransomNote.js
+++ ransomNote.js
@@ -16,18 +16,16 @@ var ransomNote = function (desiredMsg, wordsToWorkWith) {
   });
 
   desiredMsgArray.forEach(desiredWord => {
-    //check if each word is present in wordsToWorkWithArray
-    for (var availableWord in wordsToWorkWithObj) {
-      //if word is present, decrement the value by 1 to keep track of how many we have left before we run
-      if (desiredWord === availableWord) {
-        wordsToWorkWithObj[availableWord]--;
-        noteIsPossible = true;
-      }
-      //if there are no more words left of the desired word then return false since it'd be impossible to create the desired message 
-      if (wordsToWorkWithObj[availableWord] <= 0) {
+    //if word is present, decrement the value by 1 to keep track of how many we have left before we run
+    if (wordsToWorkWithObj[availableWord]) {
+      wordsToWorkWithObj[availableWord]--;
+      if (wordsToWorkWithObj[availableWord] < 0) {
         noteIsPossible = false;
       }
+    } else {
+      noteIsPossible = false;
     }
+
   });
   console.log('Note Is Possible: ' + noteIsPossible);
 };
