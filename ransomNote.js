fbb44d849347f90d7042506c23ef11298a55b3f7
antoabrand <aaandrade1987@gmail.com>
2017-12-16 19:15:24 -0600

    adding html file to load script and working on ransomeNote

--- ransomNote.js
+++ ransomNote.js
@@ -1,14 +1,27 @@
-function ransomNote(desiredMsg, wordsToWorkWith) {
-  var desiredMsgArray = desiredMsg.split(" ");
-  var wordsToWorkWithArray = wordsToWorkWith.split(" ");
+var ransomNote = function (desiredMsg, wordsToWorkWith) {
+  var desiredMsgArray = desiredMsg.split(' ');
+  var wordsToWorkWithArray = wordsToWorkWith.split(' ');
   var wordsToWorkWithObj = {};
 
   wordsToWorkWithArray.forEach(word => {
-    if (!wordsToWorkWithObj[word]) wordsToWorkWithObj.map(word=> 0);
+    if (!wordsToWorkWithObj[word]) {
+      wordsToWorkWithObj[word] = 0;
+    }
     wordsToWorkWithObj[word]++;
   });
 
-  console.log(wordsToWorkWithObj);
-}
-
-ransomNote('', 'How many times is times a times');
+  desiredMsgArray.forEach(desiredWord => {
+    //check if each word is present in wordsToWorkWithArray
+    for (var availableWord in wordsToWorkWithObj) {
+      if (availableWord === desiredWord) {
+        wordsToWorkWithObj[availableWord]--;
+        console.log( wordsToWorkWithObj[availableWord]);
+      }
+    }
+    if (wordsToWorkWithObj[availableWord] <= 0)
+      return console.log('Not enough words, sorry :(');
+    else {
+      return console.log("You're good!");
+    }
+  });
+};
